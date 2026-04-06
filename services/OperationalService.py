e5dd8eceb95029ba91cf36a8f624dfa3844e66d9
chris-ludwig <chris.cm.ludwig@gmail.com>
2026-04-06 17:14:54 -0400

    update logging, add viewing system health

--- services/OperationalService.py
+++ services/OperationalService.py
@@ -1,30 +1,43 @@
 from models.SystemHealth import SystemHealth
 from models.LogInformation import LogInformation
 from providers.LogDataProvider import LogDataProvider
+import psutil
 import datetime
 
 class OperationalService:
     def __init__(self, log_provider: LogDataProvider):
         self.log_provider = log_provider
 
-    def log_event(self, user_id: str, message: str) -> bool: #Not sure how to get the user email from user id right now
-        log = LogInformation(
+    def log_event(self, user_id: str, message: str, email: str) -> bool:
+        log = LogInformation(#create LogInfo
             log_id="",
             user_id=user_id,
             log_message=message,
             time=int(datetime.datetime.now().timestamp()),
-            email="..."
+            email=email
         )
-        try:
+        try:#save log to DB
             id=self.log_provider.save_log(log)
-            if id:
+            if id:#check for successful save
                 return True
             return False
         except Exception:
             return False
 
     def get_system_health(self) -> SystemHealth:
-        pass 
+        #retrieve data
+        up_time = int((datetime.datetime.now()-datetime.datetime.fromtimestamp(psutil.boot_time())).total_seconds())
+        memory_usage = psutil.virtual_memory().percent
+        disk_space = psutil.disk_usage('/').percent
+        cpu_usage = psutil.cpu_percent(interval=1)
+        health = SystemHealth(#create health object
+            up_time=up_time,
+            memory_usage=memory_usage,
+            disk_space=disk_space,
+            cpu_usage=cpu_usage
+        )
+
+        return health
 
     def get_all_logs(self) -> list[LogInformation]:
         return self.log_provider.get_all_logs()
